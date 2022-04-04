6a302ef575789552a1f20870662034485da4f869
Kenichi Yasukata <kyasukata@outlook.com>
2022-04-04 18:46:02 +0900

    adapt example app for changed hook init arguments

--- apps/basic/main.c
+++ apps/basic/main.c
@@ -1,30 +1,24 @@
 #include <stdio.h>
-#include <stdint.h>
-#include <unistd.h>
-#include <errno.h>
-#include <syscall.h>
 
-#define __syscall_ret_errno(...)	    \
-	({				    \
-		long ret;		    \
-		errno = 0;		    \
-		ret = syscall(__VA_ARGS__); \
-		if (errno) {		    \
-			ret = -errno;	    \
-		}			    \
-		ret;			    \
-	})
+typedef long (*syscall_fn_t)(long, long, long, long, long, long, long);
 
-long __hook_fn(int64_t a1, int64_t a2, int64_t a3,
-	       int64_t a4, int64_t a5, int64_t a6,
-	       int64_t a7)
+static syscall_fn_t next_sys_call = NULL;
+
+static long hook_function(long a1, long a2, long a3,
+			  long a4, long a5, long a6,
+			  long a7)
 {
-	printf("output from __hook_fn: syscall number %ld\n", a1);
-	return __syscall_ret_errno(a1, a2, a3, a4, a5, a6, a7);
+	printf("output from hook_function: syscall number %ld\n", a1);
+	return next_sys_call(a1, a2, a3, a4, a5, a6, a7);
 }
 
-int __hook_init(void)
+int __hook_init(long placeholder __attribute__((unused)),
+		void *sys_call_hook_ptr)
 {
 	printf("output from __hook_init: we can do some init work here\n");
+
+	next_sys_call = *((syscall_fn_t *) sys_call_hook_ptr);
+	*((syscall_fn_t *) sys_call_hook_ptr) = hook_function;
+
 	return 0;
 }
